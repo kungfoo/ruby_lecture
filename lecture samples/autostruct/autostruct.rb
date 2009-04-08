95093e2ca63ab95bf15beee1cf14e40690528031
Silvio Heuberger <silvio.heuberger@hsr.ch>
2009-04-08 11:51:03 +0200

    tried few things on autostruct, still failing

--- lecture samples/autostruct/autostruct.rb
+++ lecture samples/autostruct/autostruct.rb
@@ -1,12 +1,30 @@
 class AutoStruct
+
   def initialize(hash = nil)
+    @properties = Hash.new
     if(hash)
-      
+      hash.each{ |key,value| @properties[key] = value }
     end
   end
   
   def method_missing(method, *args)
     puts "#{method} called, args: #{args}"
-    nil
+    
+    if method =~ /=$/
+      set_property(method, *args)
+    else
+      get_property(method)
+    end
+  end
+  
+  private
+  def set_property(name, *args)
+    name.gsub!(/=/, "")
+    @properties[name] = args
+  end
+  
+  def get_property(name)
+    puts @properties
+    @properties[name]
   end
 end
--- lecture samples/autostruct/test/autostruct_test.rb
+++ lecture samples/autostruct/test/autostruct_test.rb
@@ -3,12 +3,15 @@ require 'autostruct.rb'
 
 class AutoStructTest < Test::Unit::TestCase
   def test_creation_with_hash
-    struct = AutoStruct.new({:key => "key", :value => "30"})
-    
+    struct = AutoStruct.new({"key" => "foo", "value" => "30"})
   end
   
   def test_creation_without_hash
     struct = AutoStruct.new
-    assert_equal(nil, struct.foo)   
+    assert_equal(nil, struct.foo)
+    
+    struct.foo = "foo is great"
+    
+    assert_equal("foo is great", struct.foo)  
   end
 end
