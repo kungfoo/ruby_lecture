3bcb05c4c726e308f39ec6697102887684666e15
Silvio Heuberger <silvio.heuberger@hsr.ch>
2009-04-20 16:54:52 +0200

    added finder method by name. this is educational ugliness. we will refactor this code to use dynamic lookup!

--- lecture samples/objectspace/person.rb
+++ lecture samples/objectspace/person.rb
@@ -3,18 +3,20 @@ class Person
     @name, @lastname = name, lastname
   end
   
-  def Person.find_by_name
-    
+  def Person.find_by_name(name)
+    found = nil
+    ObjectSpace.each_object(Person) do |p|
+      found = p if p.name = name
+    end
+    return found
   end
   
   def Person.find_by_lastname
   end
   
   def Person.find_by_nameAndLastname
-    
   end
   
   def Person.find_by_nameOrLastName
-    
   end
 end
