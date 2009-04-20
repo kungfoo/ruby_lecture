4f0effbebab6335de4622d79d35cbbfb1d9df1ff
Silvio Heuberger <silvio.heuberger@hsr.ch>
2009-04-20 16:49:28 +0200

    initial version of the person class with finder methods.

---
+++ lecture samples/objectspace/person.rb
@@ -0,0 +1,20 @@
+class Person
+  def initialize(name, lastname)
+    @name, @lastname = name, lastname
+  end
+  
+  def find_by_name
+    
+  end
+  
+  def find_by_lastname
+  end
+  
+  def find_by_nameAndLastname
+    
+  end
+  
+  def find_by_nameOrLastName
+    
+  end
+end
