23d239ee775c42e3ae0e26dceb19fb3e8e765bb3
Silvio Heuberger <silvio.heuberger@hsr.ch>
2009-04-21 09:54:01 +0200

    the rivers be drying...

--- lecture samples/objectspace/test/person_test.rb
+++ lecture samples/objectspace/test/person_test.rb
@@ -3,19 +3,28 @@ require "test/unit"
 require "person.rb"
 
 class TestPerson < Test::Unit::TestCase
+  def setup
+    @person = Person.new 1,2
+  end
+  
   def test_find_by_name
-    a = Person.new 1,2
     found = Person.find_by_name(1)
-    
     assert_not_nil(found)
-    assert_equal(a, found)
+    assert_equal(@person, found)
   end
   
   def test_find_by_lastname
-    a = Person.new 1,2
     found = Person.find_by_lastname(2)
-    
     assert_not_nil(found)
-    assert_equal(a, found)
+    assert_equal(@person, found)
+  end
+  
+  def test_find_by_name_and_lastname
+    found = Person.find_by_name_and_lastname(3,3)
+    assert_nil(found)
+    
+    found = Person.find_by_name_and_lastname(1,2)
+    assert_not_nil(found)
+    assert_equal(@person, found)
   end
 end
