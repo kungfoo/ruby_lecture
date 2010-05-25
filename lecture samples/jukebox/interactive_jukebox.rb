5bfeaa7753a2c32d3327ec69995b2c373aa4d6a9
Silvio Heuberger <silvio.heuberger@hsr.ch>
2010-05-25 08:19:52 +0200

    extracted method

--- lecture samples/jukebox/interactive_jukebox.rb
+++ lecture samples/jukebox/interactive_jukebox.rb
@@ -5,7 +5,20 @@ require "cdplayer.rb"
 
 player = CdPlayer.new
 jukebox = Jukebox.new(player)
-jukebox_methods = (jukebox.methods - Object.new.methods).sort
+
+def print_help(jukebox)
+  jukebox_methods = (jukebox.methods - Object.new.methods).sort
+  puts "Jukebox Commands:"
+  jukebox_methods.each do |method|
+    param_count = jukebox.method(method).arity
+    params = ''
+    if param_count > 0
+      params = '(' << ('a'..'z').to_a[0,param_count].join(', ') << ')'
+    end
+      
+    puts("  #{method}#{params}")
+  end
+end
 
 loop do
   cmd = Readline.readline("jukebox >> ", true)
@@ -15,16 +28,7 @@ loop do
     exit!
     
   when /^(help|\?)$/
-    puts "Jukebox Commands:"
-    jukebox_methods.each do |method|
-      param_count = jukebox.method(method).arity
-      params = ''
-      if param_count > 0
-        params = '(' << ('a'..'z').to_a[0,param_count].join(', ') << ')'
-      end
-      
-      puts("  #{method}#{params}")
-    end
+    print_help(jukebox)
   
   else
     begin
